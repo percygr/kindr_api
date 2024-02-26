e05cdff1c385f6421bf96366bc2ae92a8a9c4545
Percy <percyg@gmail.com>
2024-02-26 19:39:20 +0000

    slideshow for email images

--- cms.js
+++ cms.js
@@ -25,3 +25,22 @@ closeButtons.forEach(function(btn) {
         this.parentElement.style.display = "none";
     }
 });
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  slides[slideIndex-1].style.display = "block";  
+  //setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
