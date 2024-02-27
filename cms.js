ce687d1ebbdfef626bb927ac1ec23e19f752679c
Percy <percyg@gmail.com>
2024-02-27 19:01:55 +0000

    fixed comments

--- cms.js
+++ cms.js
@@ -1,27 +1,18 @@
 // Get all modal images
 var modalImages = document.querySelectorAll(".modal-image");
-// Loop through each modal image
 modalImages.forEach(function (img, index) {
-  // Add click event listener to each modal image
   img.onclick = function () {
-    // Get the corresponding modal for the clicked image
     var modal = document.querySelectorAll(".modal")[index];
-    // Get the corresponding modal content for the clicked image
     var modalImg = document.querySelectorAll(".modal-content")[index];
-    // Show the modal
     modal.style.display = "flex";
-    // Set the image source in the modal
     modalImg.src = this.src;
   };
 });
 
 // Get all close buttons
 var closeButtons = document.querySelectorAll(".close");
-// Loop through each close button
 closeButtons.forEach(function (btn) {
-  // Add click event listener to each close button
   btn.onclick = function () {
-    // Hide the modal
     this.parentElement.style.display = "none";
   };
 });
